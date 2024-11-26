b796510603b2122a8b4275798eb7d102acb90a50
Rushikesh-P37 <kingrushii20@gmail.com>
2024-11-27 00:29:52 +0530

    Intial Commit

--- src/test/java/casestudy/MavenJenkinsTest.java
+++ src/test/java/casestudy/MavenJenkinsTest.java
@@ -2,8 +2,13 @@ package casestudy;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
 
 public class MavenJenkinsTest {
