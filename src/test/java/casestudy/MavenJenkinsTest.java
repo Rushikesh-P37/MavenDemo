bb248a9b918876c4ba0a6b6af8cbb46a8aae2215
Rushikesh-P37 <kingrushii20@gmail.com>
2024-11-27 00:13:24 +0530

    Updated the code.

--- src/test/java/casestudy/MavenJenkinsTest.java
+++ src/test/java/casestudy/MavenJenkinsTest.java
@@ -22,6 +22,29 @@ public class MavenJenkinsTest {
 		driver.manage().window().maximize(); 
 		driver.get("https://in.bookmyshow.com/");
 	}
+
+	@Test
+	public void searchCity() {
+		
+		//1 way-Using JavaScriptExecutor Finding the total number of iframes
+		JavascriptExecutor exe = (JavascriptExecutor) driver;
+		Integer noOfFrames= Integer.parseInt(exe.executeScript("return window.length").toString());
+		System.out.println("Number of frames are: "+noOfFrames);
+		
+		//2 way-Finding the total number of elements that have the tag iframe
+//		List <WebElement> iframeElements = driver.findElements(By.tagName("iframe"));
+//		System.out.println("Total number of iframe are: "+ iframeElements.size());
+		
+		driver.switchTo().defaultContent();
+	
+		WebElement search = driver.findElement(By.xpath("//input[@value='']"));
+		Actions action = new Actions(driver);
+		action.sendKeys("Pune");
+		action.sendKeys(search,Keys.DOWN);
+		action.sendKeys(search,Keys.ENTER);
+		action.perform();
+		
+	}
 	
 
 	@AfterTest
